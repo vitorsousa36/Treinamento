6101b01b2c457e629915f5d7d889a984f3c495ad
vitor <vitor@DESKTOP-9LRQGPF>
2019-01-06 01:18:14 -0200

    Adicionando a atividade 2

---
+++ Treinamento1/src/br/com/Atividades/Atividade2.java
@@ -0,0 +1,41 @@
+package br.com.Atividades;
+
+import java.util.Scanner;
+
+public class Atividade2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+//Primeira solucao
+//		int num1;
+//		int num2;
+		
+//			for(num1 =1; num1<9; num1++) {
+//				System.out.println("Tabuada de " + num1);
+//				for(num2 = 1; num2<10; num2++) {
+//					System.out.println(num1+" X "+num2 +" = "+num1*num2 );
+//				} 				
+//		}
+		
+		
+//		Segunda solução		
+//		System.out.println("Entre com o numero da tabuada");
+//		int entrada = scan.nextInt();
+//		
+//		int mult = 0;
+//		
+//		while (mult < 10) {
+//		
+//			int result  = entrada * mult;
+//			System.out.println(entrada+" X "+mult+" = "+result);
+//            mult++;
+//
+//		}
+		
+		
+	  
+	}
+
+}
